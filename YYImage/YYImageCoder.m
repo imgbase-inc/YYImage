267b58a50fbf7a3b19f2f8c2efcbd71fd1c1d345
Jeon Hyuncheol <julyfool@gmail.com>
2021-06-15 17:51:04 +0900

    📦 레거시 코드 대응

--- YYImage/YYImageCoder.m
+++ YYImage/YYImageCoder.m
@@ -15,8 +15,7 @@
 #import <ImageIO/ImageIO.h>
 #import <Accelerate/Accelerate.h>
 #import <QuartzCore/QuartzCore.h>
-#import <MobileCoreServices/MobileCoreServices.h>
-#import <AssetsLibrary/AssetsLibrary.h>
+#import <CoreServices/CoreServices.h>
 #import <objc/runtime.h>
 #import <pthread.h>
 #import <zlib.h>
@@ -2790,23 +2789,6 @@ CGImageRef YYCGImageCreateWithWebPData(CFDataRef webpData,
 
 - (void)setYy_isDecodedForDisplay:(BOOL)isDecodedForDisplay {
     objc_setAssociatedObject(self, @selector(yy_isDecodedForDisplay), @(isDecodedForDisplay), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (void)yy_saveToAlbumWithCompletionBlock:(void(^)(NSURL *assetURL, NSError *error))completionBlock {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        NSData *data = [self _yy_dataRepresentationForSystem:YES];
-        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-        [library writeImageDataToSavedPhotosAlbum:data metadata:nil completionBlock:^(NSURL *assetURL, NSError *error){
-            if (!completionBlock) return;
-            if (pthread_main_np()) {
-                completionBlock(assetURL, error);
-            } else {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    completionBlock(assetURL, error);
-                });
-            }
-        }];
-    });
 }
 
 - (NSData *)yy_imageDataRepresentation {
